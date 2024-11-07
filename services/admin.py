b85189824fa60440c280afd2631819959c39986b
nasi_day <157891907+Nasi1945@users.noreply.github.com>
2024-11-07 13:49:38 +0330

    data base services

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Attribute,service
 # Register your models here.
+
+admin.site.register(Attribute)
+admin.site.register(service)
--- services/models.py
+++ services/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Attribute(models.Model):
+    name = models.CharField(max_length=120)
+    content = models.TextField(default='کمک کننده')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('created_at',)
+        
+        
+class service(models.Model):
+    name = models.CharField(max_length=120)
+    content = models.TextField(default='این سرویس کار شما را راحت تر کرده است')
+    image = models.ImageField(upload_to='services', default='service.jpg')
+    attribute = models.ManyToManyField(Attribute)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('created_at',)
+    
+    
